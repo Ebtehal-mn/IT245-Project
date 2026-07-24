0dfc7d639e0d765b04e09b83dd305b47b4a91be5
Fatema-alruwaie <S240062218@seu.edu.sa>
2026-07-24 19:35:04 +0300

    Implement VistorHashMap class for visitor management
    Added VistorHashMap class to manage visitor records with methods to add, search, update, remove, and display visitors.

--- main.java
+++ main.java
@@ -37,6 +37,60 @@ puplic class VisitorStack {
   
 
 //Student 2
+import java.util.HashMap;
+public class VistorHashMap { 
+  //creat a HashMap to store visitor name
+  private HashMap<string, string> visitors;
+  public visitorHashMap() {
+    visitors = new Hashmap<>();
+  }
+  // Add a visitor 
+  //Time complexty:O(1)
+  public void addVisitor(string visitorName, string visitTime) {
+    visitors.put(visitorName, visitTime); 
+    System.out.printin(visitorName + " was added to the visitor records."); 
+  }
+// search for a visitor
+  public void searchVisitor(string visitorName) {
+    if (visitors.containsKey(visitorName) {
+      System.out.printin(visitorName + " visited at:" + visitoers.get(visitorName));
+    }
+    else { 
+      System.out.printin("visitor not found.");
+    }
+  }
+  //update visitor information
+  public void updeteVisitor(String visitorName, String NewVisitTime) {
+    if (visitors.containsKey(visitorName)) {
+      visitors.put(visitorName, NewVisitTime);
+      System.out.printin(visitorName + "information updated.");
+    } else{
+      System.out.printin("Visitor not found")
+        }
+  }
+  //Remove a visitor
+  public void removeVisitor(String visitorName) {
+    if (visitors.containsKey(VisitorName)) {
+      visitors.remove(visitorName);
+      System.out.printin(visitorName + " removed successfully.");
+    } else {
+      System.out.printin("Visitor not found.");
+    }
+  }
 
+  //Display all visitors
+  public void displayVisitors() {
+    if (visitors.isEmpty()) {
+      System.out.printin("No visitors found.");
+      return;
+    }
+    
+    System.out.prinin("Visitor records:");
 
+    for (String name : visitors.keySet()) {
+      System.out.printin(name + " -> " + visitors.get(name));
+    }
+  }
+}
+    
 //Student 3
