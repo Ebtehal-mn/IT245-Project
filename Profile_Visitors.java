639f38cfade220d36b30aa03534eeea7102ed427
Ebtehal <121483213+Ebtehal-mn@users.noreply.github.com>
2026-07-26 21:48:43 +0300

    Add main method for testing the solutions
    Implemented main method to test two visitor approaches using Stack and HashMap.

--- Profile_Visitors.java
+++ Profile_Visitors.java
@@ -1,1 +1,24 @@
 
+public class Profile_Visitors {
+  public static void main(String[] args) {//this is the main method to call and test the Two approaches we selected for the project
+    System.out.println("\m=== Recent Profile Visitors Project ===\n");
+    System.out.println("\m=== 1. Using Stack Solution ===\n");
+    Visitorstack stackSolution = new Visitorstack();
+    //Adding new visitors using Stack so;ution
+    stackSolution.addVisitor("Sara");
+    stackSolution.addVisitor("Ahmed");
+    stackSolution.addVisitor("Noura");
+
+
+
+
+
+
+    
+    System.out.println("\m================================================\n");
+    System.out.println("\m=== 2. Using HashMap Solution ===\n");
+
+
+    
+  }
+}
