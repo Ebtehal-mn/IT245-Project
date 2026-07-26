e31c08131a20b450f2880501eca538ce5722eded
Fatema-alruwaie <S240062218@seu.edu.sa>
2026-07-26 22:38:22 +0300

    Add VisitorHashMap class with visitor management
    Implement VisitorHashMap class to manage visitor entries with a maximum capacity.

--- VistorHashMap.java
+++ VistorHashMap.java
@@ -1,1 +1,17 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
 
+public class VisitorHashMap {
+  private static final int MAX_VISITORS = 5; //define the capacity of the list
+private LinkedHashMap<String, String> visitors;
+
+public VisitorHashMap() {
+  visitors = new LinkedHashMap<>();
+}
+// adding a visitor, for linkedHashMap put operation is O(1)
+public void addVisitor(String visitorName, String visitTime) {
+  //this if is to check if the name is already in the list
+   if (visitors.containsKey(visitorName)) {
+     visitors.remove(visitorName); //here it will delet it to be able to add it at last one
+   }
+  
